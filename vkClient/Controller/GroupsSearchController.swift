da069f35f9f6aba8bea3f28cd4ea16e6d18c7fb6
guseynov <rslnguseynov@gmail.com>
2019-07-02 11:40:49 +0300

    complete L2

---
+++ vkClient/Controller/GroupsSearchController.swift
@@ -0,0 +1,13 @@
+//
+//  GroupsSearchController.swift
+//  vkClient
+//
+//  Created by Ruslan Guseynov on 02/07/2019.
+//  Copyright © 2019 Ruslan Guseynov. All rights reserved.
+//
+
+import UIKit
+
+class GroupsSearchController: UITableViewController {
+    
+}
